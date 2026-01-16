16a767d76486795175842187717d78248b179a93
Utkarsh750 <sharmautkarsh750.snp@gmail.com>
2026-01-16 13:11:00 +0530

    added redis db

--- server/index.js
+++ server/index.js
@@ -6,9 +6,14 @@ import todoRouter from "./routes/todo.js";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { Redis } from "ioredis";
+import axios from "axios";
+
 dotenv.config();
 
 const app = express();
+
+const redisClient = new Redis();
 const PORT = process.env.PORT || 5000;
 connectDB();
 
@@ -22,6 +27,22 @@ app.use(
   })
 );
 
+app.get("/post", async (req, res) => {
+  try {
+    const cachedData = await redisClient.get("posts");
+    if (cachedData !== null) {
+      return res.json(JSON.parse(cachedData));
+    } else {
+      const { data } = await axios(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      await redisClient.set("posts", JSON.stringify(data));
+      return res.json(data);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/todo", todoRouter);
 
